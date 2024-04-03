80f7eeae8529ef929a268f235cc82763a86cdbbb
Wasimultonmoy <79182688+Wasimultonmoy@users.noreply.github.com>
2024-04-03 21:28:34 +0600

    Added Exe

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/USER/Documents/MatrixCalculator
+# Install script for directory: C:/Users/USER/Documents/GitHub/Matrix-Calculator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/USER/Documents/MatrixCalculator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/USER/Documents/GitHub/Matrix-Calculator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
